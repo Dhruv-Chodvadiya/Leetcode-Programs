40698ec3da089ad3bf5db37001950a9894a73d0b
Dhruv <chodvadiyadhruv06@gmail.com>
2023-10-14 20:36:26 +0530

    169 commit

---
+++ Leetcode_JAVA/Majority_Element_169.java
@@ -0,0 +1,24 @@
+public class Majority_Element_169 {
+    public static void main(String[] args) {
+        int[] arr={3,2,3};
+        int count=0;
+        int i;
+        int num=0;
+        for(i=0;i<arr.length;i++){
+            int a=arr[i];
+            int temp=0;
+            for(int j=i;j<arr.length;j++){
+                if(a==arr[j]){
+                    temp++;
+                }
+            }
+            if(temp>count){
+                count=temp;
+                num=arr[i];
+
+            }
+        }
+//        return num;
+        System.out.println(num);
+    }
+}
